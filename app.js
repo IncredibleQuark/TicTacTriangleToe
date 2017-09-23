16338e63e64b32608e5cddbc37619648de6cb76f
IncredibleQuark <lkupinski@gmail.com>
2017-09-23 16:46:02 +0200

    added board and working on communication

--- app.js
+++ app.js
@@ -1,46 +1,182 @@
-var express = require('express'),
-    app = express();
-    serv = require('http').Server(app);
+// var express = require('express'),
+//     app = express();
+//     serv = require('http').Server(app);
+//
+//
+//
+//     app.get('/', function(req, res) {
+//
+//         res.sendFile(__dirname + '/client/index.html');
+//
+//     });
+//
+//     app.use('/client', express.static(__dirname + '/client'));
+//
+//     serv.listen(2000);
+//     console.log('Server start');
+//
+//
+//     var SOCKET_LIST = {};
+//     var PLAYER_LIST = {};
+//
+//     var Player = function (id) {
+//         var moves = [];
+//
+//         var self = {
+//             id: id,
+//             moves: moves
+//         };
+//
+//
+//         self.updateMoves = function (move) {
+//             moves.push(move)
+//         };
+//         return self;
+//     };
+//
+//     var io = require('socket.io') (serv, {});
+//
+//     io.sockets.on('connection', function (socket) {
+//
+//         socket.id = Math.random();
+//         socket.number = Math.floor(10 * Math.random());
+//         SOCKET_LIST[socket.id] = socket;
+//
+//
+//         player = Player(socket.id);
+//         // if (Object.keys(PLAYER_LIST).length === 0) {
+//         //
+//         //      mark = '<div class="x"><div class="x"></div></div>';
+//         //      player = Player(socket.id, mark);
+//         // }
+//         // else if (Object.keys(PLAYER_LIST).length === 1) {
+//         //
+//         //     mark = '<div class="circle"></div>';
+//         //     player = Player(socket.id, mark);
+//         // }
+//
+//         PLAYER_LIST[socket.id] = player;
+//
+//         socket.on('newMark', function (data) {
+//
+//
+//             player.updateMoves(data.position);
+//
+//         });
+//         // socket.on('newMark', function (data) {
+//         //
+//         //     for(var i in SOCKET_LIST) {
+//         //         var socket = SOCKET_LIST[i];
+//         //
+//         //         socket.emit('move', {
+//         //             box: data.position,
+//         //             number: socket.number,
+//         //             mark: PLAYER_LIST[socket].mark
+//         //         });
+//         //     }
+//         //
+//         // });
+//
+//         setInterval(function () {
+//
+//             var pack = [];
+//
+//             for (var i in PLAYER_LIST) {
+//                 var player = PLAYER_LIST[i];
+//
+//                 pack.push({
+//                     mark: player.mark,
+//                     position: player.position,
+//                     moves: player.moves
+//                 });
+//             }
+//
+//             for (var j in SOCKET_LIST) {
+//                 var socket = SOCKET_LIST[j];
+//                 socket.emit('move', pack);
+//             }
+//
+//         }, 1000/25);
+//
+//
+//         socket.on('disconnect', function () {
+//             delete SOCKET_LIST[socket.id];
+//             delete PLAYER_LIST[socket.id];
+//         })
+//     });
+
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Server started.");
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){
+    var self = {
+        x:250,
+        y:250,
+        id:id,
+        move: '',
+        mark: '',
+        number:"" + Math.floor(10 * Math.random()),
+        pressingRight:false,
+        pressingLeft:false,
+        pressingUp:false,
+        pressingDown:false,
+        maxSpd:10
+    };
+
+    return self;
+};
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+
+    socket.id = Math.random();
+    SOCKET_LIST[socket.id] = socket;
+
+    var player = Player(socket.id);
+    PLAYER_LIST[socket.id] = player;
+
+    socket.on('disconnect',function(){
+        delete SOCKET_LIST[socket.id];
+        delete PLAYER_LIST[socket.id];
+    });
+
+    socket.on('newMark', function(data){
+        var pack = [];
+        player.move = data.box;
+        for(var i in PLAYER_LIST){
+            player = PLAYER_LIST[i];
+            pack.push({
+                x:player.x,
+                y:player.y,
+                move: player.move,
+                number:player.number
+            });
+        }
+        for(var j in SOCKET_LIST){
+            var socket = SOCKET_LIST[j];
+            socket.emit('move',pack);
+        }
 
 
 
-    app.get('/', function(req, res) {
-
-        res.sendFile(__dirname + '/client/index.html');
 
     });
 
-    app.use('/client', express.static(__dirname + '/client'));
 
-    serv.listen(2000);
-    console.log('Server start');
+});
 
-
-    var SOCKET_LIST = {};
-
-    var io = require('socket.io') (serv, {});
-
-    io.sockets.on('connection', function (socket) {
-
-        socket.id = Math.random();
-        SOCKET_LIST[socket.id] = socket;
-
-        // socket.on('disconnect', function() {
-        //     delete SOCKET_LIST[socket_id];
-        // })
-
-        socket.on('newMark', function (data) {
-
-            for(var i in SOCKET_LIST) {
-                var socket = SOCKET_LIST[i];
-
-                socket.emit('move', {
-                    box: data.position
-                });
-            }
-
-        })
-    });
 
 
 
